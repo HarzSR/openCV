a437a5b9ebd54825cf56045fad55e84677e56aa5
Hariharan Thiagarajan <hariharansmm@gmail.com>
2021-03-04 15:45:58 +1300

    Fourth Commit
    Live Video Resize

--- rescale.py
+++ rescale.py
@@ -1,12 +1,24 @@
 import cv2 as cv
 
 def rescaleFrame(frame, scale=0.75):
+    # Works for imnages, videos and live video
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
     dimensions = (width,height)
 
     return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+def changeRes(width, height):
+    # Works for live video only
+    capture.set(3.width)
+    capture.set(4,height)
+
+img = cv.imread('Photos/cat_large.jpg')
+img_resize = rescaleFrame(img)
+
+cv.imshow('Cat', img)
+cv.imshow('Cat Resized', img_resize)
 
 capture = cv.VideoCapture('Videos/dog.mp4')
 
