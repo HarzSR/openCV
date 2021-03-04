07078a435ad037b15f506086b0393a33a7bb1e9c
Hariharan Thiagarajan <hariharansmm@gmail.com>
2021-03-04 14:40:46 +1300

    Second Commit
    Read video frame by frame and display video
    
    -215: Assertion failed Error - It will come when Python can no longer find the file or frame it need next

--- read.py
+++ read.py
@@ -1,7 +1,23 @@
 import cv2 as cv
 
-img = cv.imread('Photos/cat.jpg')
+# Reading Images
 
-cv.imshow('Cat', img)
+# img = cv.imread('Photos/cat_large.jpg')
 
-cv.waitKey(0)
+# cv.imshow('Cat', img)
+
+# cv.waitKey(0)
+
+# Reading Videos
+
+capture = cv.VideoCapture('Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
